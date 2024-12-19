17b59d25cd9ef0d061ea8f1ee05841b99757b4ce
Abhinav <abhiseggyam@gmail.com>
2024-12-19 15:39:46 +0530

    Adding two files

---
+++ test.py
@@ -0,0 +1,2 @@
+print("The first line of the first file")
+print("The second line of the first file")
---
+++ test1.py
@@ -0,0 +1,2 @@
+print("first line of the second file")
+print("Second line of the second file")
